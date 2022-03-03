265cc7eec20dc94bb72642907555e1838221058d
sondossamii <sondos.sami.abuzeid@gmail.com>
2022-03-03 22:19:08 +0200

    Add 'Ratings' Carousel and its section, and Footer

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+// $(document).ready(function () {
+//   $(".overview-carousel").owlCarousel({
+//     loop: true,
+//     dots: true,
+//     responsive: {
+//       0: {
+//         items: 1,
+//       },
+//     },
+//   });
+// });
+
+// $(document).ready(function () {
+//   $(".feedback-carousel").owlCarousel({
+//     loop: true,
+//     dots: true,
+//     responsive: {
+//       0: {
+//         items: 1,
+//       },
+//     },
+//   });
+// });
+
 $(document).ready(function () {
-  $(".overview-carousel").owlCarousel({
+  $(".owl-carousel").owlCarousel({
     loop: true,
     dots: true,
     responsive: {
