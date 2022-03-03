0601ec44314eea4fea604e997e62f165bb7eabb6
sondossamii <sondos.sami.abuzeid@gmail.com>
2022-03-03 23:53:38 +0200

    Add some style to media queries

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-// $(document).ready(function () {
-//   $(".overview-carousel").owlCarousel({
-//     loop: true,
-//     dots: true,
-//     responsive: {
-//       0: {
-//         items: 1,
-//       },
-//     },
-//   });
-// });
-
-// $(document).ready(function () {
-//   $(".feedback-carousel").owlCarousel({
-//     loop: true,
-//     dots: true,
-//     responsive: {
-//       0: {
-//         items: 1,
-//       },
-//     },
-//   });
-// });
-
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
